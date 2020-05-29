292fbefbeb4dbad3c41c22b9f07ca9f695f3030c
Zh <64052158+windcrazy2000@users.noreply.github.com>
2020-05-29 10:02:57 +0800

    测试

---
+++ src/main/java/cn/edu/scujcc/GameboxApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.scujcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameboxApplication.class, args);
+	}
+
+}
