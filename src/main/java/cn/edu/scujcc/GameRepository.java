0539c4eb51ffd59caa00e48ae88bd1a2ed9c141d
Zh <64052158+windcrazy2000@users.noreply.github.com>
2020-06-05 10:14:01 +0800

    添加Repository和Service，并完善代码

---
+++ src/main/java/cn/edu/scujcc/GameRepository.java
@@ -0,0 +1,11 @@
+package cn.edu.scujcc;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface GameRepository extends MongoRepository<Game,String>{
+
+}
---
+++ src/main/java/cn/edu/scujcc/GameService.java
@@ -0,0 +1,76 @@
+package cn.edu.scujcc;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import ch.qos.logback.classic.Logger;
+
+
+@Service
+public class GameService {
+	private GameRepository repo;
+	public static final Logger logger = (Logger) LoggerFactory.getLogger(GameService.class);
+	
+	/**
+	 * 获取一个游戏
+	 */
+	public Game getGame(String gameId) {
+		logger.debug("准备从数据库读取数据"+gameId);
+		Optional<Game> result = repo.findById(gameId);
+		
+		if(result.isPresent()) {
+			return result.get();
+		}else {
+		return null;
+		}
+	}
+	
+	/**
+	 * 获取所有游戏
+	 */
+	public List<Game> getAllGames(){
+		logger.debug("准备从数据库读取所有频道信息...");
+		return repo.findAll();
+	}
+	
+	/**
+	 * 删除指定游戏
+	 */
+	public boolean deleteGame(String gameId) {
+		boolean result = true;
+		repo.deleteById(gameId);
+		
+		return result;
+	}
+	
+	/**
+	 * 更新一个游戏
+	 */
+	public Game updateGame(Game c) {
+		Game saved = getGame(c.getId());
+		if (saved != null) {
+			if (c.getTitle() != null) {
+				saved.setTitle(c.getTitle());
+			}
+			if (c.getPrice() != null) {
+				saved.setPrice(c.getPrice());
+			}
+					
+		}
+		if(c.getCover()!=null) {
+			saved.setCover(c.getCover());
+		}
+		return repo.save(saved);
+	}
+	
+	/**
+	 * 创建
+	 */
+	public Game createChannel(Game c) {
+		return repo.save(c);
+		
+	}
+}
